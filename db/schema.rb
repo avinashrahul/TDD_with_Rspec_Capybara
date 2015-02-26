d753caac0af988db427314f4a369dc686fb2d40d
avinashrahul <avinash.sastra@gmail.com>
2015-02-25 23:09:02 -0500

    Allow a user to create a todo spec

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150224012414) do
+ActiveRecord::Schema.define(version: 20150226034850) do
 
   create_table "books", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "todos", force: true do |t|
+    t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
